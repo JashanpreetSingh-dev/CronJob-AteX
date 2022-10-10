0d10fbad2f83e838dc4eb0e8d593dcde0c719fc2
JashanpreetSingh-dev <77453449+JashanpreetSingh-dev@users.noreply.github.com>
2022-10-10 14:53:48 -0700

    Change the interval

--- cronjob.py
+++ cronjob.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from main import write_data
 
 scheduler = BlockingScheduler()
-scheduler.add_job(write_data, "interval", seconds=10000000000)
+scheduler.add_job(write_data, "interval", seconds=30)
 scheduler.start()
